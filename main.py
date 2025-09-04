dd1aa6de7f2c4ba4dc73309420bcaefc5323c949
Skoutperiferia16 <Skoutperiferia16@Skandia.com.co>
2025-09-04 15:55:49 -0500

    Se actualiza gptvitalemocional

--- main.py
+++ main.py
@@ -4338,18 +4338,18 @@ def generate_dashboard(individual_charts, consolidated_chart,usuario_id):
     # Obtener interpretaciones de ChatGPT para cada categoría
     logging.info(f"Archivos recibidos en individual_charts: {individual_charts}")
     ai_interpretations = {}
-    # for categoria in categorias:
-    #     if categoria in promedios and categoria in dimension_scores:
-    #       interpretation = get_chatgpt_interpretation(
-    #         categoria,
-    #         promedios[categoria],
-    #         dimensiones[categoria],
-    #         dimension_scores[categoria]
-    #      )
-    #       ai_interpretations[categoria] = interpretation or "Interpretación no disponible"
-    #     else:
-    #      logging.warning(f"No hay datos completos para la categoría {categoria}")
-    #      ai_interpretations[categoria] = "Datos no disponibles para esta categoría"
+    for categoria in categorias:
+        if categoria in promedios and categoria in dimension_scores:
+          interpretation = get_chatgpt_interpretation(
+            categoria,
+            promedios[categoria],
+            dimensiones[categoria],
+            dimension_scores[categoria]
+         )
+          ai_interpretations[categoria] = interpretation or "Interpretación no disponible"
+        else:
+         logging.warning(f"No hay datos completos para la categoría {categoria}")
+         ai_interpretations[categoria] = "Datos no disponibles para esta categoría"
 
     # Datos de interpretación para los tooltips
     interpretaciones = {
